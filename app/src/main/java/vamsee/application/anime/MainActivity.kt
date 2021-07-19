7021bf48b8c85c4bdfeed6737a5b0b4eec8a4d48
vamseevk <vamseevk2001@gmail.cmo>
2021-07-19 22:30:59 +0530

    Initial commit

---
+++ app/src/main/java/vamsee/application/anime/MainActivity.kt
@@ -0,0 +1,11 @@
+package vamsee.application.anime
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
